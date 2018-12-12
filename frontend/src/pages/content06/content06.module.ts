8d40cd6e0ce9aacaa9e6a3593840c89d7757f6c8
Wuchenyu6 <fan.a.fan@qq.com>
2018-12-12 14:38:17 +0800

    添加了content01-06（推荐详情页）

---
+++ frontend/src/pages/content06/content06.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Content06Page } from './content06';
+
+@NgModule({
+  declarations: [
+    Content06Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Content06Page),
+  ],
+})
+export class Content06PageModule {}
