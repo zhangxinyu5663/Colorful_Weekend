d51c0bfbb5abc0ef08402dbe6f64b198f9e1f233
Wuchenyu6 <fan.a.fan@qq.com>
2018-12-18 15:21:46 +0800

    点赞、收藏页更改

--- frontend/src/pages/collection/collection.ts
+++ frontend/src/pages/collection/collection.ts
@@ -12,5 +12,17 @@ export class CollectionPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   goMy(){this.navCtrl.push(MyPage);}
-
+  // num;
+  // btn;
+  // flag=true;
+  // change(i){
+  //   this.btn=document.getElementsByClassName('btn')[i];
+  //   if(this.flag==true){
+  //     this.num=i;
+  //     this.btn.style.color="aliceblue";
+  //   }else{
+  //     this.btn.style.color="yel";
+  //   }
+  //   this.flag=!this.flag;
+  // }
 }
