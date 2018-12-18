ba08dba25cafee6060b65c9d99b1bd57c0c9f8f8
zxy5663 <1976871412@qq.com>
2018-12-18 10:12:25 +0800

    修改set页

--- frontend/src/pages/set/set.ts
+++ frontend/src/pages/set/set.ts
@@ -15,10 +15,30 @@ export class SetPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  goBack(){this.navCtrl.push(MyPage);}
+  // goBack(){this.navCtrl.push(MyPage);}
+  goBack(){this.navCtrl.pop();}
   goChangePw(){this.navCtrl.push(ChangepwPage);}
   goSetdata(){this.navCtrl.push(SetdataPage);}
   goAccount(){this.navCtrl.push(AccountPage);}
   goPhonenum(){this.navCtrl.push(PhonenumPage);}
   goLogin(){this.navCtrl.push(LoginPage);}
+
+  // ionViewDidEnter(){
+  //   let elements = document.querySelectorAll(".tabbar");
+  //   if (elements != null) {
+  //      Object.keys(elements).map((key) => {
+  //         elements[key].style.display = 'none';
+  //     });
+  //   }   
+  // }
+  //ionic当退出页面的时候触发的方法
+  // ionViewWillLeave() {
+  //   let elements = document.querySelectorAll(".tabbar");
+  //   if (elements != null) {
+	//     Object.keys(elements).map((key) => {
+  //   		elements[key].style.display = 'flex';
+	//     });
+  //   }
+  // }
+
 }
