1f33d7e82de4c4808beceff502f0af083534c0fc
zxy5663 <1976871412@qq.com>
2018-12-21 20:24:01 +0800

    修改添加作品页

--- frontend/src/pages/add/add.ts
+++ frontend/src/pages/add/add.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PicturePage } from '../picture/picture';
 import { ReleaseonePage } from '../releaseone/releaseone';
+import { HttpClient } from '@angular/common/http';
 /**
  * Generated class for the AddPage page.
  *
@@ -16,13 +17,22 @@ import { ReleaseonePage } from '../releaseone/releaseone';
 })
 export class AddPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPage');
-  }
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad AddPage');
+  // }
   goSub(){
     this.navCtrl.push(ReleaseonePage);
+  }
+  userID; //用户ID
+  myPublish=[];
+  ionViewWillEnter(){
+    this.userID=localStorage.getItem('id');
+    this.http.post('/api/userPublish',{userID:this.userID}).subscribe(data=>{
+      this.myPublish=Array.prototype.slice.call(data);
+      console.log(this.myPublish);
+    });
   }
 }
