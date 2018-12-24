3b8cb80cf1a11cb85871e6748522599752865f39
yangmeijing <1806102644@qq.com>
2018-12-24 16:05:03 +0800

    首页推荐作品详情

--- frontend/src/pages/content01/content01.ts
+++ frontend/src/pages/content01/content01.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { Slides } from "ionic-angular";
 import { ViewChild } from "@angular/core";
 import { AlertController } from 'ionic-angular';
+import { ChangeDetectorRef } from '@angular/core';
+import * as $ from 'jquery';
 
 /**
  * Generated class for the Content01Page page.
@@ -18,41 +20,90 @@ import { AlertController } from 'ionic-angular';
 })
 export class Content01Page {
   @ViewChild(Slides) slides: Slides;
+  
   homedetailID;//用来记录页面跳转的是哪个作品的详情
-  obj;
+  obj;//请求的这个作品
   userID;//标记此时是哪个用户登录
-  context;//
+  context;//作品详细介绍的内容
   imgs=[];//用来盛放作品图片数组
-  constructor(private alertCtrl: AlertController,public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public modalCtrl:ModalController,public cd: ChangeDetectorRef,private alertCtrl: AlertController,public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
   }
-  homeDetail;
-  homeComments;
+  homeDetail;//作品详情
+  homeComments;//作品评论
   ionViewWillEnter(){
     
   }
   ionViewDidLoad(){
      //获得当前用户登录的userID。
-     this.userID=localStorage.getItem('id')
+     localStorage.setItem('userID',"234567");
+     this.userID=localStorage.getItem('userID');
      this.homedetailID=localStorage.getItem('homedetailID');
+
+     //请求作品详情
      this.http.post('/api/homedetail',{id:this.homedetailID}).subscribe(data=>{
        this.homeDetail=data['detail'][0];
        this.homeComments=data['comments'];
-       console.log(this.homeComments);
        this.imgs=this.homeDetail.imgs.split("|");
        this.obj=this.homeDetail;
        this.context=this.obj.context;
+       console.log(this.context);
        this.show();
      })
   }
   ionViewDidEnter(){
+    // var that=this;
+    // $(".reply_btn").click(function reply(RowGuid){
+    //   console.log(RowGuid);
+    //   $("#reply_textarea").remove();
+    //   $(this).parent().append("<div id='reply_textarea'><textarea id='textarea' cols='45' rows='4'></textarea><br/><button id='publish'>发表</button></div>");
+    //   var reply_textarea=document.getElementById('reply_textarea');
+    //   reply_textarea.style.cssText = "width:100%;height:80px;text-align:center;"
+    //   var textarea = document.getElementById('textarea');
+    //   textarea.setAttribute("placeholder","快写下你的评论吧~");
+    //   textarea.style.cssText="margin-top:15px;"
+    //   var pub=document.getElementById('publish');
+    //   pub.style.cssText = "display:block;float:right;";
+    //   console.log(pub);
+    //   //btn.addEventListener("click",that.button); 
+    //   pub.addEventListener("click",function(){
+    //     var textarea = document.getElementById('textarea');
+    //     var text=textarea.innerHTML;
+    //     if(text==''){
+    //       that.presentAlert();
+    //     }else{
+    //       that.http.post('/api/addcomment',{RowGuid:RowGuid,userID:that.userID,context:text,projectID:that.homedetailID}).subscribe(data=>{
+    //         console.log(data);
+    //       });
+    //     }
+    //   },false);
+    // })
+    // $(".reply_btn").click(function(RowGuid)
     
     // this.allcomment();
   }
-  
+
+  //弹出评论框
+  reply_textarea;
+  textarea;
+  publish;
+  reply(RowGuid,i){
+    console.log(RowGuid,i);
+    this.reply_textarea=document.getElementsByClassName('reply_textarea');
+    console.log(this.reply_textarea);
+    this.reply_textarea[i].style.display="block";
+  };
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: '评论不能为空',
+      // subTitle: '10% of battery remaining',
+      buttons: ['确认']
+    });
+    alert.present();
+  }
+
 //点击展开全部和收起的函数
-show()
-{ 
-  if(typeof this.context != "undefined"){
+show(){ 
     var box = document.getElementById("box");
     var text = this.context;
     var newBox = document.createElement("div"); 
@@ -76,15 +127,14 @@ show()
     box.innerHTML = ""; 
     box.appendChild(newBox); 
     box.appendChild(btn); 
-    } 
-
+    
 } 
-
+//退出这个界面
   isColor:boolean = true;
   close(){
     this.navCtrl.pop();
   }
-
+//关注此用户
   change(){
     this.isColor=!this.isColor;
     if (this.isColor == true) {
@@ -94,41 +144,46 @@ show()
         document.getElementById('btn').innerText = "已关注";
     }
   }
-  //弹出回复评论窗口
-  presentPrompt(RowGuid) {
-    let alert = this.alertCtrl.create({
-      title: 'Reply',
-      inputs: [
-        {
-          name: 'reply',
-          placeholder: '你想回复啥~'
-        }
-      ],
-      buttons: [
-        {
-          text: '取消',
-          role: 'cancel',
-          handler: data => {
-            console.log(data);
-          }
-        },
-        {
-          text: '发表',
-          handler: data => {
-            this.http.post('/api/addcomment',{RowGuid:RowGuid,userID:this.userID,context:data.reply,projectID:this.homedetailID}).subscribe(data=>{
-                console.log(data);
-            });
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
+
+ //收藏此作品
+ collection;//代表点击的这个收藏图标
+ collect(){
+    this.collection=document.getElementById('collection');
+    var isclick=this.collection.getAttribute("isclick");
+    console.log(isclick);
+    var collectionNum;
+    if(isclick == "true"){
+      this.collection.style.color="yellow";
+      this.collection.style.size="17px";
+      collectionNum=parseInt(document.getElementById('collectionNum').innerText);
+      collectionNum=collectionNum+1;
+      console.log(collectionNum);
+      document.getElementById('collectionNum').innerText=collectionNum;
+      isclick=!Boolean(isclick);
+      console.log(String(isclick));
+      this.collection.setAttribute('isclick',String(isclick));
+      this.http.post('/api/homedetail/collect',{collectionNum:collectionNum,userID:this.userID,projectID:this.homedetailID}).subscribe(data=>{
+        console.log(data);
+      })
+    }
+    else{
+      this.collection.style.color="black";
+      collectionNum=parseInt(document.getElementById('collectionNum').innerText);
+      collectionNum=collectionNum-1;
+      document.getElementById('collectionNum').innerText=collectionNum;
+      isclick="true";
+      this.collection.setAttribute('isclick',isclick);
+      this.http.post('/api/homedetail/delcollect',{collectionNum:collectionNum,userID:this.userID,projectID:this.homedetailID}).subscribe(data=>{
+        console.log(data);
+    })
+    }        
+ }
+  
 //展开全部评论函数(暂未想出来)。
   allcomment(){
     var liNum = document.getElementsByClassName('inner')[0].children;
     console.log(liNum);
-    var replyButton=document.getElementById('replyButton');
+    // var replyButton=document.getElementById('replyButton');
 
     // if(liNum==0){
     //   replyButton.style.display="none";
