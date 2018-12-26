edaa22a891bd312c8c6a4e4a901e25c7ae21202c
yangmeijing <1806102644@qq.com>
2018-12-26 08:58:17 +0800

    用户的个人信息介绍

--- frontend/src/pages/userinfo/userinfo.ts
+++ frontend/src/pages/userinfo/userinfo.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FollowpersonPage } from '../followperson/followperson';
+import { HttpClient } from '@angular/common/http';
 
 @IonicPage()
 @Component({
@@ -9,8 +10,19 @@ import { FollowpersonPage } from '../followperson/followperson';
 })
 export class UserinfoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
   }
   goBack(){this.navCtrl.popTo(FollowpersonPage);}
- 
+
+  userdetailID;//当前关注用户id
+  userInfo;//用户详细信息
+  ionViewWillEnter(){
+    this.userdetailID=localStorage.getItem('userIDdetail');
+    console.log(this.userdetailID);
+    this.http.post('/api/UserInfo',{userID:this.userdetailID}).subscribe(data=>{
+        this.userInfo=data['userInfo'][0];
+        console.log(this.userInfo);
+        console.log(this.userInfo.head);
+    })
+  }
 }
