bbe98f4115dd298857bc360d744f7070954ad1fb
yangmeijing <1806102644@qq.com>
2019-01-01 17:03:55 +0800

    用户作品详情

---
+++ backend/src/pages/userproject/userproject.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { UserprojectPage } from './userproject';
+
+@NgModule({
+  declarations: [
+    UserprojectPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(UserprojectPage),
+  ],
+})
+export class UserprojectPageModule {}
---
+++ backend/src/pages/userproject/userproject.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * Generated class for the UserprojectPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-userproject',
+  templateUrl: 'userproject.html',
+})
+export class UserprojectPage {
+
+  constructor(public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  projectID;//作品id
+  project;//作品
+  imgs;//作品图
+  context;
+  ionViewWillEnter(){
+    this.projectID=localStorage.getItem('projectdetailID');
+    this.http.post('/api/searchfive/projectID',{projectID:this.projectID}).subscribe(data=>{
+      this.project=Array.prototype.slice.call(data)[0];
+      this.imgs=this.project.imgs.split("|");
+      console.log(this.imgs);
+      console.log(this.project.number);
+    })
+  }
+  ionViewDidLoad(){
+    
+  }
+  //返回上一页
+  back(){
+    this.navCtrl.pop();
+  }
+}
