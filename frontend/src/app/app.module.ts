5b2091bdd1dcf5a6c6bc656ed33a06acefe1fc9e
zxy5663 <1976871412@qq.com>
2019-05-29 08:37:31 +0800

    更新配置

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,13 +17,6 @@ import {ImagePicker,ImagePickerOptions} from "@ionic-native/image-picker";
 
 import { AddPage } from '../pages/add/add';
 import { PlanonePage } from '../pages/planone/planone';
-import { PlantwoPage } from '../pages/plantwo/plantwo';
-import { PlanthreePage } from '../pages/planthree/planthree';
-import { PlanfourPage } from '../pages/planfour/planfour';
-import { PlanfivePage } from '../pages/planfive/planfive';
-import { PlansixPage } from '../pages/plansix/plansix';
-import { PlansevenPage } from '../pages/planseven/planseven';
-import { PlaneightPage } from '../pages/planeight/planeight';
 import { CalendarPage } from '../pages/calendar/calendar';
 import { GuanzhuPage } from '../pages/guanzhu/guanzhu';
 import { FollowpersonPage } from '../pages/followperson/followperson';
@@ -48,16 +41,13 @@ import { Forgetpwd2Page } from '../pages/forgetpwd2/forgetpwd2';
 import { ChangepwdalertPage } from '../pages/changepwdalert/changepwdalert';
 import { Register2Page } from '../pages/register2/register2';
 import { UserinfoPage } from '../pages/userinfo/userinfo';
-import { Content01Page } from '../pages/content01/content01';
 import { EditmypublishPage } from '../pages/editmypublish/editmypublish';
-import { ReleasetwoPage } from '../pages/releasetwo/releasetwo';
 import { SearchPage } from '../pages/search/search';
-import { SearchtwoPage } from '../pages/searchtwo/searchtwo';
 import { EditmyschedulePage } from '../pages/editmyschedule/editmyschedule';
 import { CommentPage } from '../pages/comment/comment';
 import { AtPage } from '../pages/at/at';
-
-
+import {HomeDetailPage} from '../pages/homedetail/homedetail';
+import {SearchresultPage} from '../pages/searchresult/searchresult';
 
 @NgModule({
   declarations: [
@@ -69,15 +59,7 @@ import { AtPage } from '../pages/at/at';
     MyPage,
     AddPage,
     AtPage,
-    Content01Page,
     PlanonePage,
-    PlantwoPage,
-    PlanthreePage,
-    PlanfourPage,
-    PlanfivePage,
-    PlansixPage,
-    PlansevenPage,
-    PlaneightPage,
     CalendarPage,
     GuanzhuPage,
     FollowpersonPage,
@@ -102,11 +84,11 @@ import { AtPage } from '../pages/at/at';
     ChangepwdalertPage,
     UserinfoPage,
     EditmypublishPage,
-    ReleasetwoPage,
     SearchPage,
-    SearchtwoPage,
+    SearchresultPage,
     EditmyschedulePage,
-    CommentPage
+    CommentPage,
+    HomeDetailPage,
   ],
   imports: [
     BrowserModule,
@@ -128,15 +110,7 @@ import { AtPage } from '../pages/at/at';
     MyPage,
     AddPage,
     AtPage,
-    Content01Page,
     PlanonePage,
-    PlantwoPage,
-    PlanthreePage,
-    PlanfourPage,
-    PlanfivePage,
-    PlansixPage,
-    PlansevenPage,
-    PlaneightPage,
     CalendarPage,
     GuanzhuPage,
     FollowpersonPage,
@@ -162,12 +136,12 @@ import { AtPage } from '../pages/at/at';
     UserinfoPage,
     EditmypublishPage,
     UserinfoPage,
-    ReleasetwoPage,
     SearchPage,
-    SearchtwoPage,
+    SearchresultPage,
     EditmypublishPage,
     EditmyschedulePage,
-    CommentPage
+    CommentPage,
+    HomeDetailPage,
   ],
   providers: [
     StatusBar,
