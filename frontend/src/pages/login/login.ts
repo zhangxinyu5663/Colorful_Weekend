31f892a519ec5db521d8dba9abbbe43a7427307f
zxy5663 <1976871412@qq.com>
2019-05-29 08:47:19 +0800

    修改登录页

--- frontend/src/pages/login/login.ts
+++ frontend/src/pages/login/login.ts
@@ -39,35 +39,35 @@ export class LoginPage {
    
   login(){   //登录验证功能
     this.inp=document.getElementsByClassName('inp')
-    if(this.inp[0].value==''){
-      const alert4= this.alertCtrl.create({
+    if(this.inp[0].value=='' || this.inp[1].value==''){
+      const alert1= this.alertCtrl.create({
         title: '登录失败',
-        subTitle: '手机号信息不能为空',
+        subTitle: '请输入完整的登录信息',
         buttons: ['OK']
       });
-      alert4.present();
+      alert1.present();
     }else{
       this.http.post('/api/login',{"phone":this.phone,"pwd":this.pwd}).subscribe(data=>{
         this.arr=data;
         console.log(this.arr);
         if(this.arr.status==0){
-          const alert3 = this.alertCtrl.create({
+          const alert2= this.alertCtrl.create({
             title: '登录失败',
             subTitle: '该手机号还未被注册',
             buttons: ['OK']
           });
-          alert3.present();
+          alert2.present();
           this.inp=document.getElementsByClassName('inp');
           console.log(this.inp);
           this.inp[1].value='';       
         }
         if(this.arr.status==1){ //账号被冻结
-          const alert1 = this.alertCtrl.create({
+          const alert3 = this.alertCtrl.create({
             title: '登录异常',
             subTitle: '你的账号已被冻结，请您遵守我们的协议并且发表正当的言论，您的账号将在一个月之后解冻',
             buttons: ['OK']
           });
-          alert1.present();
+          alert3.present();
           this.inp=document.getElementsByClassName('inp');
           this.inp[0].value='';
           this.inp[1].value='';
@@ -80,12 +80,12 @@ export class LoginPage {
         }
         if(this.arr.status==3){ //密码错误
           //弹框提示
-          const alert2 = this.alertCtrl.create({
+          const alert4 = this.alertCtrl.create({
             title: '登录失败',
             subTitle: '请输入正确的密码信息',
             buttons: ['OK']
           });
-          alert2.present();
+          alert4.present();
           this.inp=document.getElementsByClassName('inp');
           console.log(this.inp);
           this.inp[1].value='';
@@ -96,24 +96,6 @@ export class LoginPage {
     
   }
 
-  // reminder(){ //手机号输入错误题型  
-  //   this.inp=document.getElementsByClassName('inp');
-  //   var r1=document.getElementById('reminder1');
-  //   //var r2=document.getElementById('reminder2');
-  //   if(this.phone=='' || this.inp[0].value.length!=11){
-  //     console.log('手机号错误');
-  //     r1.innerHTML='!&nbsp;请填写正确的手机号';
-  //   }
-  // }
-  // reset(){ //重置函数 将span里的内容和输入框里的内容置为空
-  //   console.log('reset');
-  //   this.inp=document.getElementsByClassName('inp');
-  //   var r1=document.getElementById('reminder1');
-  //   //var r2=document.getElementById('reminder2');
-  //   r1.innerHTML='';
-  //   this.inp[0].value='';
-  //   //r2.innerHTML='';
-  // }
   forgetPwd(){ //忘记密码
     this.navCtrl.push(ForgetpwdPage);
   }
