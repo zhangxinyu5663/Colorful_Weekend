bb61d24e2a7312224013171f763cb4518189e397
zxy5663 <1976871412@qq.com>
2019-06-05 08:20:32 +0800

    增加我关注的用户作品详情页

---
+++ frontend/src/pages/attentionuserproject/attentionuserproject.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AttentionuserprojectPage } from './attentionuserproject';
+
+@NgModule({
+  declarations: [
+    AttentionuserprojectPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AttentionuserprojectPage),
+  ],
+})
+export class AttentionuserprojectPageModule {}
---
+++ frontend/src/pages/attentionuserproject/attentionuserproject.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GuanzhuPage } from '../guanzhu/guanzhu';
+import { UserinfoPage } from '../userinfo/userinfo';
+import { HttpClient } from '@angular/common/http';
+import { HomeDetailPage } from '../homedetail/homedetail';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-attentionuserproject',
+  templateUrl: 'attentionuserproject.html',
+})
+export class AttentionuserprojectPage {
+  constructor(public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
+  }
+  goGuanzhu(){this.navCtrl.popTo(GuanzhuPage)}
+  
+  userdetailID;//点开的用户的id
+  userdetail;//关注的用户详情数组
+  userHead;//用户头像
+  userName;//用户名称
+  userID;//用户ID
+  ionViewWillEnter(){
+    this.userdetailID=localStorage.getItem('userIDdetail');
+    console.log(this.userdetailID);
+    this.http.post('/api/my/userDetail',{userID:this.userdetailID}).subscribe(data=>{
+        this.userdetail=data['userDetail'];
+        console.log(this.userdetail);
+        this.userHead=this.userdetail[0].head;
+        console.log(this.userHead);
+        this.userName=this.userdetail[0].userName;
+        this.userID=this.userdetail[0].userID;
+    })
+  }
+
+  detail(projectID){
+    localStorage.setItem('homedetailID',projectID);
+    this.navCtrl.push(HomeDetailPage);
+  }
+//查看用户详情
+  goUserinfo(){
+    this.navCtrl.push(UserinfoPage);
+  }
+}
